8562dfd86f99b71cf53f49525fe394bc36011066
matthinc <matthias.rupp@posteo.de>
2017-09-18 21:49:35 +0200

    Clean up tray module

--- tray.js
+++ tray.js
@@ -1,78 +1,80 @@
 const Client = require('node-rest-client').Client
-
 var client = new Client()
 
-module.exports = function(Tray, Menu, hass, password) {
+/**
+ * Calls a home assistant service
+ * @param {*} hass 
+ * @param {*} password 
+ * @param {*} domain 
+ * @param {*} service 
+ * @param {*} entity_id 
+ */
+function hassService(hass, password, domain, service, entity_id) {
+    client.post(`${hass}/api/services/${domain}/${service}`,
+        { headers: { 'x-ha-access': password, 'Content-Type': 'application/json' }, data: { entity_id: entity_id, } },
+        (data, res) => { })
+}
+
+/**
+ * get single domain and create best label
+ * @param {*} states 
+ * @param {*} domain 
+ * @param {*} sensor if true, label contains state and unit
+ */
+function filterDomain(states, domain, sensor = false) {
+    return states
+        .filter(state => state.entity_id.startsWith(domain + '.'))
+        .map(item => {
+            let label = item.attributes.friendly_name
+            if (!label) {
+                label = item.entity_id
+            }
+            if (sensor) {
+                label += ': ' + item.state
+            }
+            if (item.attributes.unit_of_measurement) {
+                label += item.attributes.unit_of_measurement
+            }
+            item.label = label
+            return item
+        })
+}
+
+function createTray(Tray, Menu, hass, password) {
     let tray = new Tray('assets/tray.png')
 
-    client.get(hass + `/api/states?api_password=${password}`, (data,res) => {
+    client.get(hass + `/api/states?api_password=${password}`, (data, res) => {
 
-        /**
-         * Switchtes
-         */
-        let switches = data.filter(state => state.entity_id.startsWith('switch.'))
-        .map(item => {
+        let switches = filterDomain(data, 'switch').map(item => {
             return {
-                label: (item.attributes.friendly_name != undefined?item.attributes.friendly_name: item.entity_id),
-                click() {
-                    //Call 'toggle' service
-                    client.post(hass +`/api/services/switch/toggle`,
-                        {headers: {'x-ha-access': password,'Content-Type': 'application/json'},data: {entity_id: item.entity_id,}
-                        },(data, res) => {})
-                }
+                label: item.label,
+                click() { hassService(hass, password, 'switch', 'toggle', item.entity_id) }
             }
         })
 
-        /**
-         * Lights
-         */
-        let lights = data.filter(state => state.entity_id.startsWith('light.'))
-        .map(item => {
+        let lights = filterDomain(data, 'light').map(item => {
             return {
-                label: (item.attributes.friendly_name != undefined?item.attributes.friendly_name: item.entity_id),
-                click() {
-                    //Call 'toggle' service
-                    client.post(hass +`/api/services/light/toggle`,
-                        {headers: {'x-ha-access': password,'Content-Type': 'application/json'},data: {entity_id: item.entity_id,}
-                        },(data, res) => {})
-                }
+                label: item.label,
+                click() { hassService(hass, password, 'light', 'toggle', item.entity_id) }
             }
         })
 
-        /*
-         * Sensors
-         */
-        let sensors = data.filter(state => state.entity_id.startsWith('sensor.'))
-        .map(item => {
+        let scenes = filterDomain(data, 'scene').map(item => {
             return {
-                label: (item.attributes.friendly_name != undefined?item.attributes.friendly_name: item.entity_id)
-                     + ': ' + item.state +
-                     (item.attributes.unit_of_measurement?item.attributes.unit_of_measurement:''),
-                enabled: false
+                label: item.label,
+                click() { hassService(hass, password, 'scene', 'turn_on', item.entity_id) }
             }
         })
 
-        /*
-         * Scenes
-         */
-        let scenes = data.filter(state => state.entity_id.startsWith('scene.'))
-        .map(item => {
-            return {
-                label: (item.attributes.friendly_name != undefined?item.attributes.friendly_name: item.entity_id),
-                click() {
-                    //Call 'toggle' service
-                    client.post(hass +`/api/services/scene/turn_on`,
-                        {headers: {'x-ha-access': password,'Content-Type': 'application/json'},data: {entity_id: item.entity_id,}
-                        },(data, res) => {})
-                }
-            }
+        let sensors = filterDomain(data, 'sensor', true).map(item => {
+            return { label: item.label, enabled: false }
         })
 
         let items = [
             ...sensors,
-            {label: 'Switches', submenu: switches},
-            {label: 'Lights', submenu: lights},
-            {label: 'Scenes', submenu: scenes}
+            { label: 'Switches', submenu: switches },
+            { label: 'Lights', submenu: lights },
+            { label: 'Scenes', submenu: scenes }
         ]
 
         let menu = Menu.buildFromTemplate(items)
@@ -81,3 +83,4 @@ module.exports = function(Tray, Menu, hass, password) {
 }
 
 
+module.exports = createTray
